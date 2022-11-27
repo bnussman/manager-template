b53698a9b17107992f5d577c78b36059de0c8d15
Banks Nussman <banks@nussman.us>
2022-11-27 11:48:53 -0500

    add comments

--- src/App.tsx
+++ src/App.tsx
@@ -12,9 +12,11 @@ function OAuth() {
   const navigate = useNavigate();
 
   useEffect(() => {
+    // When the OAuth route is mounted...
+
+    // Prase query params and get relevent data such as access_token and expires_in
     const url = new URLSearchParams(window.location.href);
     const data = Array.from(url.entries());
-
     const token = data.find(entry => entry[0].includes("access_token"))?.[1];
     const expires_in = url.get("expires_in")
 
@@ -25,20 +27,21 @@ function OAuth() {
     const expiresIn = Number(expires_in) * 1000;
     const expiresAt = Date.now() + expiresIn;
 
-    console.log("New token expires at", new Date(expiresAt));
-
+    // Make app reauth when token is set to expire
     setTimeout(() => {
       localStorage.removeItem('expires');
       localStorage.removeItem('token');
       window.location.href = encodeURI(`${authorizeUrl}?response_type=token&client_id=${clientId}&state=xyz&redirect_uri=http://localhost:5173/callback&scope=*`);
     }, expiresIn);
 
+    // Store token and expire time in localstorage
     localStorage.setItem('token', token);
     localStorage.setItem('expires', String(expiresAt));
 
-    console.log(token)
+    // Set @linode/api-v4 token for use
     setToken(token);
 
+    // Go home
     navigate("/");
   }, []);
 
@@ -54,20 +57,29 @@ function Main() {
     const hasToken = token !== null;
     const expiresAt = Number(localStorage.getItem('expires'));
 
+    // If we initialize the app with the /callback url, we need to render routes but do no processing.
     if (location.pathname.includes('/callback')) {
       setIsLoading(false);
       return;
     }
+
+    // When we initialize the app...
     if (hasToken && expiresAt > Date.now()) {
+      // If we have an auth token that is not expired...
+      // Make app redirect to auth when token expires
       setTimeout(() => {
         localStorage.removeItem('expires');
         localStorage.removeItem('token');
         window.location.href = encodeURI(`${authorizeUrl}?response_type=token&client_id=${clientId}&state=xyz&redirect_uri=http://localhost:5173/callback&scope=*`);
       }, expiresAt - Date.now());
+
+      // Set the @linode/api-v4 token and render the app
       setToken(token);
       setIsLoading(false);
       return;
     }
+
+    // if we have mde it here, we need to authenticate
     window.location.href = encodeURI(`${authorizeUrl}?response_type=token&client_id=${clientId}&state=xyz&redirect_uri=http://localhost:5173/callback&scope=*`);
   }, []);
 
